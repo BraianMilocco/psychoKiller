b7c2e4ef7daf22f63b5232d97ded838952ee1913
Lucas Botteri <lucasbotteri@hotmail.com>
2020-10-03 23:30:46 -0300

    feat: Added Inital PsycoKiller Modeling
    - Added Basic Models with relationships
    - Register Models to Admins
    - Added Model documentation

---
+++ back/psychoApp/models/option.py
@@ -0,0 +1,11 @@
+from django.db import models
+from . import Question
+
+class Option(models.Model):
+    choise = models.TextField("Opcion")
+    point = models.IntegerField("Puntos", blank=True, null=True)
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.choise
