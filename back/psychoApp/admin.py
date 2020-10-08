1ae24f20e7b6ba0f5263e32dd83d9516883defa4
Lucas Botteri <lucasbotteri@hotmail.com>
2020-10-07 21:21:14 -0300

    Added code checl pipeloine

--- back/psychoApp/admin.py
+++ back/psychoApp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Answer, Option, Patient, Question, TemplateTest, Terapist, Test
-
+def a(){
+    d = 3
+    return 'aa'
+}
 models_to_register = (Answer, Option, Patient, Question, TemplateTest, Terapist, Test)
 admin.site.register(models_to_register)
